02144a773e38b8d208a742f7f6d5ab7094199432
Ajay Bura <ajbura@gmail.com>
2022-06-22 08:41:35 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, id odio. Cumque, provident
+        voluptas iste ducimus alias ipsa dolores. Dolores officiis vitae maxime fuga consequuntur
+        eaque voluptate tenetur neque similique?
+      </p>
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index",
+  devtool: "inline-source-map",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: "./dist",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ["babel-loader", "ts-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
+};
