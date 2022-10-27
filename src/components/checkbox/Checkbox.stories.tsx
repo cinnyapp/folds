380afe9843b2d5617bd0e781e0307b99ad2aa389
ajbura <ajbura@gmail.com>
2022-10-27 16:20:36 +0530

    Add Checkbox

--- src/components/checkbox/Checkbox.stories.tsx
+++ src/components/checkbox/Checkbox.stories.tsx
@@ -6,11 +6,21 @@ import { Checkbox } from "./Checkbox";
 export default {
   title: "Checkbox",
   component: Checkbox,
+  argTypes: {
+    variant: {
+      control: "select",
+      options: ["Primary", "Secondary", "Success", "Warning", "Critical"],
+    },
+    size: {
+      control: "select",
+      options: [50, 100, 200, 300, 400, 500, 600],
+    },
+  },
 } as ComponentMeta<typeof Checkbox>;
 
 const Template: ComponentStory<typeof Checkbox> = (args) => <Checkbox {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  value: false,
+export const Secondary = Template.bind({});
+Secondary.args = {
+  defaultChecked: true,
 };
--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -1,25 +1,136 @@
-import React, { ButtonHTMLAttributes, forwardRef } from "react";
+import React, { HTMLAttributes, forwardRef } from "react";
 
-import { CSS, styled } from "../../config";
+import { CSS, styled, VariantProps } from "../../config";
 import { Icon, Icons } from "../icon";
+import { MainColor } from "../types";
 
-const StyledCheckbox = styled("button", {});
+const getVariant = (variant: MainColor) => ({
+  "input[type=checkbox]:checked": {
+    boxShadow: "none",
+    backgroundColor: `$${variant}`,
+    "& + *": {
+      color: `$On${variant}`,
+    },
+  },
+});
 
-interface CheckboxProps
-  extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "value" | "children"> {
-  value?: boolean;
+const StyledCheckbox = styled("span", {
+  padding: "0",
+  margin: "0",
+  width: "$$CheckboxSize",
+  height: "$$CheckboxSize",
+
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  position: "relative",
+
+  "input, input[type=checkbox]": {
+    appearance: "none",
+    margin: 0,
+    padding: 0,
+    border: "none",
+    cursor: "pointer",
+    borderRadius: "$300",
+    boxShadow: "inset 0 0 0 $borderWidths$400 CurrentColor",
+
+    "& + *": {
+      color: "transparent",
+      pointerEvents: "none",
+    },
+    "&:not(:checked)": {
+      opacity: "$P300",
+    },
+    "&:focus-visible": {
+      outline: "$FocusRing",
+    },
+
+    "&[disabled], &[aria-disabled=true]": {
+      opacity: "$Disabled",
+      cursor: "not-allowed",
+      "& + *": {
+        opacity: "$Disabled",
+      },
+    },
+  },
+  "& > *": {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+
+  variants: {
+    variant: {
+      Primary: getVariant("Primary"),
+      Secondary: getVariant("Secondary"),
+      Success: getVariant("Success"),
+      Warning: getVariant("Warning"),
+      Critical: getVariant("Critical"),
+    },
+    size: {
+      50: {
+        $$CheckboxSize: "$sizes$X50",
+      },
+      100: {
+        $$CheckboxSize: "$sizes$X100",
+      },
+      200: {
+        $$CheckboxSize: "$sizes$X200",
+      },
+      300: {
+        $$CheckboxSize: "$sizes$X300",
+      },
+      400: {
+        $$CheckboxSize: "$sizes$X400",
+      },
+      500: {
+        $$CheckboxSize: "$sizes$X500",
+      },
+      600: {
+        $$CheckboxSize: "$sizes$X600",
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: "Secondary",
+    size: "400",
+  },
+});
+
+type CheckboxVariant = VariantProps<typeof StyledCheckbox>;
+interface CheckboxProps extends Omit<HTMLAttributes<HTMLInputElement>, "children" | "onChange"> {
+  className?: string;
+  defaultChecked?: boolean;
+  checked?: boolean;
+  onChange?: (checked: boolean) => void;
   css?: CSS;
 }
 
-export const Checkbox = forwardRef<HTMLButtonElement, CheckboxProps>(
-  ({ value = false, ...props }, ref) => (
-    <StyledCheckbox role="checkbox" defaultChecked={false} {...props} ref={ref}>
+export const Checkbox = forwardRef<HTMLSpanElement, CheckboxVariant & CheckboxProps>(
+  ({ className, variant, size, style, css, defaultChecked, checked, onChange, ...props }, ref) => (
+    <StyledCheckbox
+      className={className}
+      variant={variant}
+      size={size}
+      style={style}
+      ref={ref}
+      css={css}
+    >
       <input
-        defaultChecked={value}
         type="checkbox"
-        onChange={(a) => console.log(a.target.checked)}
+        {...props}
+        defaultChecked={defaultChecked}
+        checked={checked}
+        onChange={
+          onChange
+            ? (evt) => {
+                onChange(evt.target.checked);
+              }
+            : undefined
+        }
       />
-      {value && <Icon src={Icons.Check} />}
+      <Icon aria-hidden size={size} src={Icons.Check} />
     </StyledCheckbox>
   )
 );
