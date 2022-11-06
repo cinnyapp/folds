44eb8f54b2a503fdccb5883aada3889bc00abfc2
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2022-11-06 17:44:03 +0530

    Update Elevation config

--- src/config.ts
+++ src/config.ts
@@ -136,10 +136,10 @@ export const {
       Max: "999",
     },
     shadows: {
-      E100: "0px 1px 2px rgba(0, 0, 0, 0.2)",
-      E200: "0px 1px 6px rgba(0, 0, 0, 0.2)",
-      E300: "0px 2px 8px rgba(0, 0, 0, 0.2)",
-      E400: "0px 4px 16px rgba(0, 0, 0, 0.2)",
+      E100: "0px 1px 6px -2px rgba(0, 0, 0, 0.2)",
+      E200: "0px 1px 12px -3px rgba(0, 0, 0, 0.2)",
+      E300: "0px 2px 16px -4px rgba(0, 0, 0, 0.2)",
+      E400: "0px 4px 26px -6px rgba(0, 0, 0, 0.2)",
     },
     sizes: {
       X50: toRem(16),
