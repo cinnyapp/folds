cb623b5240fcf18ba71932eeb0e7030e11517885
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2022-11-13 21:18:22 +0530

    Add Menu Item component

---
+++ src/components/menu/MenuItem.stories.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { MenuItem } from "./MenuItem";
+import { Text } from "../text";
+import { Icon, Icons } from "../icon";
+
+export default {
+  title: "MenuItem",
+  component: MenuItem,
+  argTypes: {
+    variant: {
+      control: "select",
+      options: [
+        "Background",
+        "Surface",
+        "SurfaceVariant",
+        "Primary",
+        "Secondary",
+        "Success",
+        "Warning",
+        "Critical",
+      ],
+    },
+  },
+} as ComponentMeta<typeof MenuItem>;
+
+const Template: ComponentStory<typeof MenuItem> = (args) => <MenuItem {...args} />;
+
+export const Surface = Template.bind({});
+Surface.args = {
+  before: <Icon size="100" src={Icons.Pencil} />,
+  children: <Text size="B400">Menu Item</Text>,
+};
---
+++ src/components/menu/MenuItem.tsx
@@ -0,0 +1,92 @@
+import React, { ElementType, ReactNode } from "react";
+import { CSS, styled, toRem, VariantProps } from "../../config";
+import { AsComponentProps, ContainerColor } from "../types";
+
+const getVariant = (variant: ContainerColor): CSS => ({
+  backgroundColor: `$${variant}Container`,
+  color: `$On${variant}Container`,
+
+  "@hover": {
+    "&:hover": {
+      backgroundColor: `$${variant}ContainerHover`,
+    },
+  },
+  "&:focus-visible": {
+    backgroundColor: `$${variant}ContainerHover`,
+    outline: "$FocusRing",
+  },
+  "&:active": {
+    backgroundColor: `$${variant}ContainerActive`,
+  },
+});
+
+const StyledMenuItem = styled("button", {
+  margin: 0,
+  appearance: "none",
+  WebkitTapHighlightColor: "transparent",
+  border: "0 solid currentColor",
+  boxShadow: "none",
+
+  height: toRem(40),
+  padding: "0 $400",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "start",
+  gap: "$200",
+  cursor: "pointer",
+
+  "&[data-ui-before=true]": {
+    paddingLeft: "$300",
+  },
+  "&[data-ui-after=true]": {
+    paddingRight: "$300",
+  },
+
+  "&[disabled], &[aria-disabled=true]": {
+    opacity: "$Disabled",
+    cursor: "not-allowed",
+  },
+
+  variants: {
+    variant: {
+      Background: getVariant("Background"),
+      Surface: getVariant("Surface"),
+      SurfaceVariant: getVariant("SurfaceVariant"),
+      Primary: getVariant("Primary"),
+      Secondary: getVariant("Secondary"),
+      Success: getVariant("Success"),
+      Warning: getVariant("Warning"),
+      Critical: getVariant("Critical"),
+    },
+  },
+  defaultVariants: {
+    variant: "Surface",
+  },
+});
+
+const defaultElement = "button";
+type MenuItemVariant = VariantProps<typeof StyledMenuItem>;
+
+type MenuItemProps<E extends ElementType = typeof defaultElement> = MenuItemVariant &
+  AsComponentProps<E> & {
+    before?: ReactNode;
+    after?: ReactNode;
+    css?: CSS;
+  };
+
+export const MenuItem = <E extends ElementType = typeof defaultElement>({
+  before,
+  after,
+  children,
+  ...props
+}: MenuItemProps<E>) => (
+  <StyledMenuItem
+    data-ui-before={before ? true : undefined}
+    data-ui-after={after ? true : undefined}
+    {...props}
+  >
+    {before}
+    {children}
+    {after}
+  </StyledMenuItem>
+);
