99271d52d0eb6c9fdb02a03666744e38efeed9ff
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-01-25 10:15:26 +0530

    refactor: use vanilla-extract in icon-button component

--- src/components/icon-button/IconButton.stories.tsx
+++ src/components/icon-button/IconButton.stories.tsx
@@ -1,163 +1,18 @@
 import React from "react";
-import { ComponentMeta } from "@storybook/react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 import { IconButton } from ".";
 import { Icon, Icons } from "../icon";
-import { Box } from "../box";
 
 export default {
   title: "IconButton",
   component: IconButton,
 } as ComponentMeta<typeof IconButton>;
 
-export const IconButton600 = () => (
-  <Box css={{ d: "flex", alI: "start", fxW: "wrap", gap: "$400" }}>
-    <IconButton size="600" variant="Background">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="600" variant="Surface">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="600" variant="SurfaceVariant">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="600" variant="Primary">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="600" variant="Secondary">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="600" variant="Success">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="600" variant="Warning">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="600" variant="Critical">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton aria-disabled size="600" variant="Surface">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton aria-pressed size="600" variant="Success">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-  </Box>
+const Template: ComponentStory<typeof IconButton> = (args) => (
+  <IconButton {...args}>
+    <Icon src={Icons.Smile} />
+  </IconButton>
 );
 
-export const IconButton500 = () => (
-  <Box css={{ d: "flex", alI: "start", fxW: "wrap", gap: "$400" }}>
-    <IconButton size="500" variant="Background">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="500" variant="Surface">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="500" variant="SurfaceVariant">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="500" variant="Primary">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="500" variant="Secondary">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="500" variant="Success">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="500" variant="Warning">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="500" variant="Critical">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-  </Box>
-);
-
-export const IconButton400 = () => (
-  <Box css={{ d: "flex", alI: "start", fxW: "wrap", gap: "$400" }}>
-    <IconButton variant="Background">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton variant="Surface">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton variant="SurfaceVariant">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton variant="Primary">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton variant="Secondary">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton variant="Success">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton variant="Warning">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton variant="Critical">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-  </Box>
-);
-
-export const IconButton300 = () => (
-  <Box css={{ d: "flex", alI: "start", fxW: "wrap", gap: "$400" }}>
-    <IconButton size="300" variant="Background">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="300" variant="Surface">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="300" variant="SurfaceVariant">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="300" variant="Primary">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="300" variant="Secondary">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="300" variant="Success">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="300" variant="Warning">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-
-    <IconButton size="300" variant="Critical">
-      <Icon src={Icons.Smile} />
-    </IconButton>
-  </Box>
-);
+export const Surface = Template.bind({});
--- src/components/icon-button/IconButton.tsx
+++ src/components/icon-button/IconButton.tsx
@@ -1,79 +1,14 @@
-import { CSS, styled, toRem } from "../../config";
-import { ContainerColor } from "../types";
+import React from "react";
+import classNames from "classnames";
+import { as } from "../as";
+import * as css from "./IconButton.css";
 
-const getVariant = (variant: ContainerColor): CSS => ({
-  backgroundColor: `$${variant}Container`,
-  color: `$On${variant}Container`,
-
-  "&[aria-pressed=true]": {
-    backgroundColor: `$${variant}ContainerActive`,
-  },
-
-  "@hover": {
-    "&:hover": {
-      backgroundColor: `$${variant}ContainerHover`,
-    },
-  },
-  "&:focus-visible": {
-    backgroundColor: `$${variant}ContainerHover`,
-    outline: "$FocusRing",
-  },
-  "&:active": {
-    backgroundColor: `$${variant}ContainerActive`,
-  },
-});
-
-export const IconButton = styled("button", {
-  padding: 0,
-  margin: 0,
-  width: "auto",
-  display: "inline-flex",
-  alignItems: "center",
-  justifyContent: "center",
-  background: "transparent",
-  border: "none",
-  boxShadow: "none",
-  lineHeight: 0,
-  color: "CurrentColor",
-  appearance: "none",
-  borderRadius: "$400",
-  cursor: "pointer",
-  textDecoration: "none",
-  WebkitTapHighlightColor: "transparent",
-
-  "&[disabled], &[aria-disabled=true]": {
-    opacity: "$Disabled",
-    cursor: "not-allowed",
-  },
-
-  variants: {
-    size: {
-      600: {
-        padding: toRem(16),
-      },
-      500: {
-        padding: toRem(12),
-      },
-      400: {
-        padding: toRem(8),
-      },
-      300: {
-        padding: toRem(4),
-      },
-    },
-    variant: {
-      Background: getVariant("Background"),
-      Surface: getVariant("Surface"),
-      SurfaceVariant: getVariant("SurfaceVariant"),
-      Primary: getVariant("Primary"),
-      Secondary: getVariant("Secondary"),
-      Success: getVariant("Success"),
-      Warning: getVariant("Warning"),
-      Critical: getVariant("Critical"),
-    },
-  },
-  defaultVariants: {
-    size: 400,
-    variant: "Surface",
-  },
-});
+export const IconButton = as<"button", css.IconButtonVariants>(
+  ({ as: AsIconButton = "button", className, size, variant, radii, outlined, ...props }, ref) => (
+    <AsIconButton
+      className={classNames(css.IconButton({ variant, size, radii, outlined }), className)}
+      {...props}
+      ref={ref}
+    />
+  )
+);
