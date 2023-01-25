5622f6b85bdbc28b96eb2c9a22d3ce33f8c16f22
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-01-25 10:19:40 +0530

    refactor: use vanilla-extract in radio-button component

--- src/components/radio-button/RadioButton.tsx
+++ src/components/radio-button/RadioButton.tsx
@@ -1,103 +1,19 @@
-import React, { forwardRef, AllHTMLAttributes } from "react";
-import { CSS, styled, VariantProps } from "../../config";
-import { MainColor } from "../types";
+import React, { AllHTMLAttributes, forwardRef } from "react";
+import classNames from "classnames";
+import * as css from "./RadioButton.css";
 
-const getVariant = (variant: MainColor): CSS => ({
-  color: `$${variant}`,
-});
+type RadioButtonProps = Omit<AllHTMLAttributes<HTMLInputElement>, "type" | "onChange" | "size"> & {
+  defaultChecked?: boolean;
+  checked?: boolean;
+};
 
-const StyledRadioButton = styled("input", {
-  "&[type=radio]": {
-    padding: "0",
-    margin: "0",
-    appearance: "none",
-    WebkitTapHighlightColor: "transparent",
-  },
-
-  width: "$$RadioButtonSize",
-  height: "$$RadioButtonSize",
-  borderRadius: "$Round",
-  boxShadow: "inset 0 0 0 $borderWidths$400 CurrentColor",
-  cursor: "pointer",
-  flexShrink: "0",
-  display: "inline-flex",
-  alignItems: "center",
-  justifyContent: "center",
-  position: "relative",
-
-  "&:checked::after": {
-    content: "",
-    position: "absolute",
-    display: "inline-block",
-    width: "60%",
-    height: "60%",
-    color: "currentColor",
-    borderRadius: "$Round",
-    backgroundColor: "currentColor",
-  },
-
-  "&:not(:checked)": {
-    opacity: "$P300",
-  },
-
-  "&:focus-visible": {
-    outline: "$FocusRing",
-  },
-
-  "&[disabled], &[aria-disabled=true]": {
-    opacity: "$Disabled",
-    cursor: "not-allowed",
-  },
-
-  variants: {
-    variant: {
-      Primary: getVariant("Primary"),
-      Secondary: getVariant("Secondary"),
-      Success: getVariant("Success"),
-      Warning: getVariant("Warning"),
-      Critical: getVariant("Critical"),
-    },
-    size: {
-      50: {
-        $$RadioButtonSize: "$sizes$X50",
-      },
-      100: {
-        $$RadioButtonSize: "$sizes$X100",
-      },
-      200: {
-        $$RadioButtonSize: "$sizes$X200",
-      },
-      300: {
-        $$RadioButtonSize: "$sizes$X300",
-      },
-      400: {
-        $$RadioButtonSize: "$sizes$X400",
-      },
-      500: {
-        $$RadioButtonSize: "$sizes$X500",
-      },
-      600: {
-        $$RadioButtonSize: "$sizes$X600",
-      },
-    },
-  },
-  defaultVariants: {
-    variant: "Secondary",
-    size: "400",
-  },
-});
-
-type RadioButtonVariant = VariantProps<typeof StyledRadioButton>;
-
-type RadioButtonProps = Omit<AllHTMLAttributes<HTMLInputElement>, "type" | "onChange"> &
-  RadioButtonVariant & {
-    defaultChecked?: boolean;
-    checked?: boolean;
-    css?: CSS;
-  };
-
-export const RadioButton = forwardRef<HTMLInputElement, RadioButtonProps>(({ ...props }, ref) => (
-  <StyledRadioButton {...props} type="radio" ref={ref} />
-));
-
-RadioButton.toString = () => StyledRadioButton.toString();
+export const RadioButton = forwardRef<HTMLInputElement, RadioButtonProps & css.RadioButtonVariants>(
+  ({ className, variant, size, ...props }, ref) => (
+    <input
+      className={classNames(css.RadioButton({ variant, size }), className)}
+      {...props}
+      type="radio"
+      ref={ref}
+    />
+  )
+);
