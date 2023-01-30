379d6f05fba32893c3985505cdd8e2f2060737c5
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-01-30 21:07:42 +0530

    build: fix build process

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import dts from "vite-plugin-dts";
+import pkg from "./package.json";
 
 export default defineConfig({
   appType: "custom",
@@ -12,10 +13,14 @@ export default defineConfig({
       entry: resolve(__dirname, "src/index.ts"),
       name: "folds",
       fileName: "index",
+      formats: ["es"],
     },
     minify: false,
     outDir: "dist",
     sourcemap: true,
     copyPublicDir: false,
+    rollupOptions: {
+      external: [...Object.keys(pkg.peerDependencies), "react/jsx-runtime"],
+    },
   },
 });
