d228fe6e0a048c4b150afd00d40f3b89c5e761ea
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-04-22 21:29:38 +1000

    fix: text variant (#15)
    * fix: priority variant
    
    * feat: container prop in protal component

--- src/components/portal/Portal.tsx
+++ src/components/portal/Portal.tsx
@@ -1,6 +1,10 @@
 import React, { ReactNode } from "react";
 import { createPortal } from "react-dom";
 
-export const Portal = ({ children }: { children: ReactNode | ReactNode[] }) => (
-  <>{createPortal(children, document.body)}</>
+export type PortalProps = {
+  container?: Element | DocumentFragment;
+  children: ReactNode | ReactNode[];
+};
+export const Portal = ({ container, children }: PortalProps) => (
+  <>{createPortal(children, container ?? document.body)}</>
 );
