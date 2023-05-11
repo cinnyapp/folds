292860dbf4e8fa0f3e034cea2729dd0da937430d
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-05-11 22:25:23 +0530

    fix: as component types

--- src/components/as.tsx
+++ src/components/as.tsx
@@ -1,9 +1,12 @@
 import { ElementType, forwardRef, ReactElement } from "react";
 import { AsComponentProps, RefOfType } from "./types";
 
-export const as = <T extends ElementType, ExtraProps = Record<string, unknown>>(
-  fc: (props: AsComponentProps<T> & ExtraProps, ref: RefOfType<T>) => ReactElement
+export const as = <T extends ElementType, ExtraProps = unknown>(
+  fc: (
+    props: Omit<AsComponentProps<T>, keyof ExtraProps> & ExtraProps,
+    ref: RefOfType<T>
+  ) => ReactElement
 ) =>
-  forwardRef(fc) as unknown as <E extends ElementType = T>(
-    props: AsComponentProps<E> & ExtraProps
+  forwardRef(fc) as unknown as <E extends ElementType>(
+    props: Omit<AsComponentProps<E>, keyof ExtraProps> & ExtraProps
   ) => ReactElement;
