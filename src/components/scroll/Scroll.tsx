8038cf994bb15c220ae77e4b9ccc8e0c318910aa
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-06-09 20:52:42 +0530

    fix: ipad scrollbar padding

--- src/components/scroll/Scroll.tsx
+++ src/components/scroll/Scroll.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import classNames from "classnames";
 import * as css from "./Scroll.css";
 import { as } from "../as";
@@ -7,20 +7,46 @@ export const Scroll = as<"div", css.ScrollVariants>(
   (
     { as: AsScroll = "div", className, variant, visibility, hideTrack, size, direction, ...props },
     ref
-  ) => (
-    <AsScroll
-      className={classNames(
-        css.Scroll({
-          variant,
-          visibility,
-          hideTrack,
-          size,
-          direction,
-        }),
-        className
-      )}
-      {...props}
-      ref={ref}
-    />
-  )
+  ) => {
+    const scrollLocalRef = useRef<HTMLDivElement>();
+
+    useLayoutEffect(() => {
+      if (scrollLocalRef.current) {
+        const $scroll = scrollLocalRef.current;
+        const xScrollbarWidth = $scroll.offsetHeight - $scroll.clientHeight;
+        const yScrollbarWidth = $scroll.offsetWidth - $scroll.clientWidth;
+
+        $scroll.setAttribute("data-x-scrollbar-width", `${xScrollbarWidth}`);
+        $scroll.setAttribute("data-y-scrollbar-width", `${yScrollbarWidth}`);
+      }
+    }, [size]);
+
+    return (
+      <AsScroll
+        className={classNames(
+          css.Scroll({
+            variant,
+            visibility,
+            hideTrack,
+            size,
+            direction,
+          }),
+          className
+        )}
+        {...props}
+        ref={(r) => {
+          if (r) scrollLocalRef.current = r;
+          if (ref) {
+            if ("current" in ref) {
+              const propRef: any = ref;
+              propRef.current = r;
+            }
+            if (typeof ref === "function") {
+              ref(r);
+            }
+          }
+        }}
+      />
+    );
+  }
 );
