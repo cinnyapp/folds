2698ebe29ed97832c17223993de8f857fd07bcc0
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-09-07 11:07:12 +0530

    fix: fix scroll corner color

--- src/components/scroll/Scroll.css.ts
+++ src/components/scroll/Scroll.css.ts
@@ -35,6 +35,9 @@ export const Scroll = recipe({
           backgroundColor: "transparent",
           borderRadius: config.radii.Pill,
         },
+        "&::-webkit-scrollbar-corner": {
+          backgroundColor: "transparent",
+        },
       },
     },
   ],
