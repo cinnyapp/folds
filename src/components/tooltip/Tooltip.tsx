ffd0a4119aba17f080fcbdd64465c70c12eaa918
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-09-28 13:44:23 +0530

    fix: adaptive popout & tooltip position

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -1,9 +1,17 @@
 import classNames from "classnames";
-import React, { MutableRefObject, ReactNode, useEffect, useRef, useState } from "react";
+import React, {
+  MutableRefObject,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import { config } from "../../theme";
 import { as } from "../as";
 import { Portal } from "../portal";
-import { Align, getRelativeFixedPosition, Position, PositionCSS } from "../util";
+import { Align, getRelativeFixedPosition, Position } from "../util";
 import * as css from "./Tooltip.css";
 
 export const Tooltip = as<"div", css.TooltipVariants>(
@@ -24,7 +32,28 @@ const useTooltip = (
   delay: number
 ) => {
   const triggerRef = useRef<unknown>(null);
-  const [tooltipPosition, setTooltipPosition] = useState<PositionCSS>();
+  const baseRef = useRef<HTMLDivElement>(null);
+  const [open, setOpen] = useState(false);
+
+  const positionTooltip = useCallback(() => {
+    const anchor = triggerRef.current as HTMLElement;
+    const baseEl = baseRef.current;
+    if (!baseEl) return;
+
+    const tooltipCss = getRelativeFixedPosition(
+      anchor.getBoundingClientRect(),
+      position,
+      align,
+      offset,
+      alignOffset,
+      baseEl.getBoundingClientRect()
+    );
+    baseEl.style.top = tooltipCss.top;
+    baseEl.style.bottom = tooltipCss.bottom;
+    baseEl.style.left = tooltipCss.left;
+    baseEl.style.right = tooltipCss.right;
+    baseEl.style.transform = tooltipCss.transform;
+  }, [position, align, offset, alignOffset]);
 
   useEffect(() => {
     const trigger = triggerRef.current as HTMLElement;
@@ -32,28 +61,22 @@ const useTooltip = (
 
     const openTooltip = (evt: Event) => {
       if (timeoutId) return;
-      const pos = getRelativeFixedPosition(
-        trigger.getBoundingClientRect(),
-        position,
-        align,
-        offset,
-        alignOffset
-      );
 
-      if (evt.type === "focus") setTooltipPosition(pos);
-      else timeoutId = window.setTimeout(() => setTooltipPosition(pos), delay);
+      if (evt.type === "focus") setOpen(true);
+      else timeoutId = window.setTimeout(() => setOpen(true), delay);
     };
+
     const closeTooltip = () => {
       clearTimeout(timeoutId);
       timeoutId = undefined;
-      setTooltipPosition(undefined);
+      setOpen(false);
     };
 
     const onKeyDown = (evt: KeyboardEvent) => {
       if (evt.key === "Escape" && document.activeElement === trigger) {
         evt.preventDefault();
         clearTimeout(timeoutId);
-        setTooltipPosition(undefined);
+        setOpen(false);
       }
     };
 
@@ -74,9 +97,14 @@ const useTooltip = (
     };
   }, [position, align, offset, alignOffset, delay]);
 
+  useLayoutEffect(() => {
+    if (open) positionTooltip();
+  }, [open, positionTooltip]);
+
   return {
-    tooltipPosition,
     triggerRef,
+    baseRef,
+    open,
   };
 };
 
@@ -105,12 +133,12 @@ export const TooltipProvider = as<"div", TooltipProviderProps>(
     },
     ref
   ) => {
-    const { tooltipPosition, triggerRef } = useTooltip(position, align, offset, alignOffset, delay);
+    const { open, triggerRef, baseRef } = useTooltip(position, align, offset, alignOffset, delay);
 
     return (
       <>
         {children(triggerRef as MutableRefObject<null>)}
-        {tooltipPosition && (
+        {open && (
           <Portal>
             <AsTooltipProvider
               role="tooltip"
@@ -118,13 +146,20 @@ export const TooltipProvider = as<"div", TooltipProviderProps>(
                 display: "inline-block",
                 position: "fixed",
                 maxWidth: "100vw",
+                maxHeight: "100vh",
                 zIndex: config.zIndex.Max,
                 pointerEvents: "none",
-                ...tooltipPosition,
                 ...style,
               }}
               {...props}
-              ref={ref}
+              ref={(el) => {
+                (baseRef as MutableRefObject<HTMLDivElement | null>).current = el;
+                if (ref) {
+                  if (typeof ref === "function") ref(el);
+                  // eslint-disable-next-line no-param-reassign
+                  else (ref as MutableRefObject<HTMLDivElement | null>).current = el;
+                }
+              }}
             >
               {tooltip}
             </AsTooltipProvider>
