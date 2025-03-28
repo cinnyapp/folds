f14644e7959c09ab39e0e9f82380b352d683a004
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2025-03-28 15:44:33 +0530

    turn off react prop-types lint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -53,6 +53,7 @@ module.exports = {
     "react/require-default-props": "off",
     "react/jsx-props-no-spreading": "off",
     "react/no-unstable-nested-components": "off",
+    "react/prop-types": "off",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "error",
 
